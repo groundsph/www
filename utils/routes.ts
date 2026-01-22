5ffa787768f8fb2d1e846510a6963a50d25cb183
Adrian Bonpin <adrianbonpin@gmail.com>
2026-01-22 22:38:04 +0800

    chore: 📝 Change /community title to "discover"

--- utils/routes.ts
+++ utils/routes.ts
@@ -18,7 +18,7 @@ export const routes: Route[] = [
         href: '/community',
         children: [
             {
-                title: 'community',
+                title: 'discover',
                 href: '/community',
             },
             {
