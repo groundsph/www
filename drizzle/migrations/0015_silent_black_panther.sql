e7476cde5b2820f4455fd39806c7d5dc5fca4c1c
Adrian Bonpin <adrianbonpin@gmail.com>
2026-04-19 22:09:55 +0800

    feat: update drizzle orm

--- drizzle/migrations/0015_silent_black_panther.sql
+++ drizzle/migrations/0015_silent_black_panther.sql
@@ -1,5 +1,6 @@
 DROP TABLE "cafe_subscriptions" CASCADE;--> statement-breakpoint
 DROP TABLE "featured_slot_requests" CASCADE;--> statement-breakpoint
 ALTER TABLE "cafes" DROP COLUMN "membership_tier";--> statement-breakpoint
-DROP TYPE "public"."membership_tier";--> statement-breakpoint
+DROP FUNCTION IF EXISTS "public"."get_cafe_tier" CASCADE;--> statement-breakpoint
+DROP TYPE "public"."membership_tier" CASCADE;--> statement-breakpoint
 DROP TYPE "public"."subscription_status";
